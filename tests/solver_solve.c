f957146d095d8b4b337b3cddd0e95b55dc02df37
Patrick Elsen <pelsen@xfbs.net>
2016-06-12 21:13:37 +0200

    adds some tests for solver_solve()

--- tests/solver_solve.c
+++ tests/solver_solve.c
@@ -2,6 +2,27 @@
 
 TEST(solve_works_with_empty_sudoku) {
     sudoku_puzzle_t puzzle = sudoku_puzzle_empty();
-    assertEquals(solve_simple(&puzzle), false);
+    sudoku_puzzle_t solved = puzzle;
+    assertEquals(solve_simple(&solved), false);
+    assertEquals(sudoku_puzzle_equals_strict(&puzzle, &solved), true);
 }
 
+TEST(solve_works_with_solved_sudoku) {
+    sudoku_puzzle_t puzzle = sudoku_puzzle_new((int[9][9]){
+            {1, 2, 3, 4, 5, 6, 7, 8, 9},
+            {4, 5, 6, 7, 8, 9, 1, 2, 3},
+            {7, 8, 9, 1, 2, 3, 4, 5, 6},
+            {2, 3, 4, 5, 6, 7, 8, 9, 1},
+            {5, 6, 7, 8, 9, 1, 2, 3, 4},
+            {8, 9, 1, 2, 3, 4, 5, 6, 7},
+            {3, 4, 5, 6, 7, 8, 9, 1, 2},
+            {6, 7, 8, 9, 1, 2, 3, 4, 5},
+            {9, 1, 2, 3, 4, 5, 6, 7, 8}});
+
+    sudoku_puzzle_t solved = puzzle;
+
+    assertEquals(sudoku_puzzle_solved(&puzzle), true);
+    assertEquals(sudoku_puzzle_valid(&puzzle), true);
+    assertEquals(solve_simple(&solved), true);
+    assertEquals(sudoku_puzzle_equals_strict(&puzzle, &solved), true);
+}
