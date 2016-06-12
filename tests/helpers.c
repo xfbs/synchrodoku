13a32000bfeb461e81782f05563c6134764f8b31
Patrick Elsen <pelsen@xfbs.net>
2016-06-12 21:12:43 +0200

    adds some helpers to compare sudokus

--- tests/helpers.c
+++ tests/helpers.c
@@ -1,2 +1,36 @@
 #include "helpers.h"
 
+bool sudoku_puzzle_equals_strict(sudoku_puzzle_t *a, sudoku_puzzle_t *b) {
+    for(int r = 0; r < 9; r++) {
+        for(int c = 0; c < 9; c++) {
+            sudoku_cell_t *a_cell = sudoku_puzzle_cell(a, r, c);
+            sudoku_cell_t *b_cell = sudoku_puzzle_cell(b, r, c);
+
+            for(int n = 0; n < 9; n++) {
+                if(a_cell->numbers[n] != b_cell->numbers[n]) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+bool sudoku_puzzle_equals(sudoku_puzzle_t *a, sudoku_puzzle_t *b) {
+    for(int r = 0; r < 9; r++) {
+        for(int c = 0; c < 9; c++) {
+            sudoku_cell_t *a_cell = sudoku_puzzle_cell(a, r, c);
+            sudoku_cell_t *b_cell = sudoku_puzzle_cell(b, r, c);
+
+            for(int n = 0; n < 9; n++) {
+                if(sudoku_cell_solution(a_cell) != sudoku_cell_solution(b_cell)) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
--- tests/helpers.h
+++ tests/helpers.h
@@ -2,3 +2,5 @@
 #include "solver.h"
 #include "sudoku.h"
 
+bool sudoku_puzzle_equals_strict(sudoku_puzzle_t *a, sudoku_puzzle_t *b);
+bool sudoku_puzzle_equals(sudoku_puzzle_t *a, sudoku_puzzle_t *b);
