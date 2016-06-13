f8e401ee2a8320ae3b84b0e6ad02b17ce43a8717
Patrick Elsen <pelsen@xfbs.net>
2016-06-13 15:48:36 +0200

    adds new test for solver

--- tests/solver_solve.c
+++ tests/solver_solve.c
@@ -56,20 +56,27 @@ TEST(solve_works_with_partially_solved_sudoku) {
 
 TEST(solve_stops_on_diverge) {
     sudoku_puzzle_t puzzle = sudoku_puzzle_new((int[9][9]){
-            {1, 2, 3, 4, 5, 6, 7, 8, 9},
-            {4, 5, 6, 7, 8, 9, 1, 2, 3},
-            {7, 8, 9, 1, 2, 3, 4, 5, 6},
-            {2, 3, 4, 5, 6, 7, 8, 9, 1},
-            {5, 6, 7, 8, 9, 1, 2, 3, 4},
-            {8, 9, 1, 2, 3, 4, 5, 6, 7},
-            {3, 4, 5, 6, 7, 8, 9, 1, 2},
-            {6, 7, 8, 9, 1, 2, 3, 4, 5},
-            {9, 1, 2, 3, 4, 5, 6, 7, 8}});
+            {0, 0, 3, 4, 5, 6, 7, 0, 9},
+            {4, 5, 6, 7, 8, 9, 0, 0, 3},
+            {7, 8, 9, 0, 0, 3, 4, 5, 6},
+            {0, 3, 4, 5, 6, 7, 8, 9, 0},
+            {5, 6, 7, 8, 9, 0, 0, 3, 4},
+            {8, 9, 0, 0, 3, 4, 0, 6, 7},
+            {3, 4, 5, 6, 7, 8, 9, 0, 0},
+            {6, 7, 8, 9, 0, 0, 3, 4, 5},
+            {9, 0, 0, 3, 4, 5, 6, 7, 8}});
 
-    sudoku_puzzle_t solved = puzzle;
+    sudoku_puzzle_t solved = sudoku_puzzle_new((int[9][9]){
+            {0, 0, 3, 4, 5, 6, 7, 8, 9},
+            {4, 5, 6, 7, 8, 9, 0, 0, 3},
+            {7, 8, 9, 0, 0, 3, 4, 5, 6},
+            {0, 3, 4, 5, 6, 7, 8, 9, 0},
+            {5, 6, 7, 8, 9, 0, 0, 3, 4},
+            {8, 9, 0, 0, 3, 4, 5, 6, 7},
+            {3, 4, 5, 6, 7, 8, 9, 0, 0},
+            {6, 7, 8, 9, 0, 0, 3, 4, 5},
+            {9, 0, 0, 3, 4, 5, 6, 7, 8}});
 
-    assertEquals(sudoku_puzzle_solved(&puzzle), true);
-    assertEquals(sudoku_puzzle_valid(&puzzle), true);
-    assertEquals(solve_simple(&solved), true);
-    assertEquals(sudoku_puzzle_equals_strict(&puzzle, &solved), true);
+    assertEquals(solve_simple(&puzzle), false);
+    assertEquals(sudoku_puzzle_equals(&puzzle, &solved), true);
 }
