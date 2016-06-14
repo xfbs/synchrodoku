1d9df287c997449f08babfacdeb4609ea86f209c
Patrick Elsen <pelsen@xfbs.net>
2016-06-14 03:39:08 +0200

    removes debug statements from solver

--- solver.c
+++ solver.c
@@ -3,10 +3,6 @@
 bool solve_simple(sudoku_puzzle_t *puzzle) {
     bool changed;
 
-    printf("solving sudoku: \n");
-    json_dumpf(sudoku_puzzle_to_json(puzzle), stdout, 0);
-    printf("\n");
-
     do {
         changed = false;
 
@@ -32,64 +28,12 @@ bool solve_simple(sudoku_puzzle_t *puzzle) {
             for(int col = 0; col < 9; col++) {
                 sudoku_cell_t *cell = sudoku_puzzle_cell(puzzle, row, col);
                 if(!sudoku_cell_solved(cell)) {
-                    printf("solving cell [%i][%i]\n", row, col);
-                    printf("cell candidates are: %c%c%c%c%c%c%c%c%c\n",
-                            cell->numbers[0] ? '1' : ' ',
-                            cell->numbers[1] ? '2' : ' ',
-                            cell->numbers[2] ? '3' : ' ',
-                            cell->numbers[3] ? '4' : ' ',
-                            cell->numbers[4] ? '5' : ' ',
-                            cell->numbers[5] ? '6' : ' ',
-                            cell->numbers[6] ? '7' : ' ',
-                            cell->numbers[7] ? '8' : ' ',
-                            cell->numbers[8] ? '9' : ' ');
-                    printf("row candidates are: %c%c%c%c%c%c%c%c%c\n",
-                            rows[row].numbers[0] ? '1' : ' ',
-                            rows[row].numbers[1] ? '2' : ' ',
-                            rows[row].numbers[2] ? '3' : ' ',
-                            rows[row].numbers[3] ? '4' : ' ',
-                            rows[row].numbers[4] ? '5' : ' ',
-                            rows[row].numbers[5] ? '6' : ' ',
-                            rows[row].numbers[6] ? '7' : ' ',
-                            rows[row].numbers[7] ? '8' : ' ',
-                            rows[row].numbers[8] ? '9' : ' ');
-                    printf("col candidates are: %c%c%c%c%c%c%c%c%c\n",
-                            cols[col].numbers[0] ? '1' : ' ',
-                            cols[col].numbers[1] ? '2' : ' ',
-                            cols[col].numbers[2] ? '3' : ' ',
-                            cols[col].numbers[3] ? '4' : ' ',
-                            cols[col].numbers[4] ? '5' : ' ',
-                            cols[col].numbers[5] ? '6' : ' ',
-                            cols[col].numbers[6] ? '7' : ' ',
-                            cols[col].numbers[7] ? '8' : ' ',
-                            cols[col].numbers[8] ? '9' : ' ');
-                    printf("square candidates are: %c%c%c%c%c%c%c%c%c\n",
-                            squares[row/3][col/3].numbers[0] ? '1' : ' ',
-                            squares[row/3][col/3].numbers[1] ? '2' : ' ',
-                            squares[row/3][col/3].numbers[2] ? '3' : ' ',
-                            squares[row/3][col/3].numbers[3] ? '4' : ' ',
-                            squares[row/3][col/3].numbers[4] ? '5' : ' ',
-                            squares[row/3][col/3].numbers[5] ? '6' : ' ',
-                            squares[row/3][col/3].numbers[6] ? '7' : ' ',
-                            squares[row/3][col/3].numbers[7] ? '8' : ' ',
-                            squares[row/3][col/3].numbers[8] ? '9' : ' ');
                     sudoku_cell_t intersect;
                     intersect = sudoku_cell_intersect(&rows[row], &cols[col]);
                     intersect = sudoku_cell_intersect(&intersect, &squares[row/3][col/3]);
                     intersect = sudoku_cell_intersect(&intersect, cell);
-                    printf("chosen candidates are: %c%c%c%c%c%c%c%c%c\n",
-                            intersect.numbers[0] ? '1' : ' ',
-                            intersect.numbers[1] ? '2' : ' ',
-                            intersect.numbers[2] ? '3' : ' ',
-                            intersect.numbers[3] ? '4' : ' ',
-                            intersect.numbers[4] ? '5' : ' ',
-                            intersect.numbers[5] ? '6' : ' ',
-                            intersect.numbers[6] ? '7' : ' ',
-                            intersect.numbers[7] ? '8' : ' ',
-                            intersect.numbers[8] ? '9' : ' ');
 
                     if(sudoku_cell_candidates(&intersect) < sudoku_cell_candidates(cell)) {
-                        printf("intersection taken!\n");
                         changed = true;
                         *cell = intersect;
                     }
@@ -97,10 +41,6 @@ bool solve_simple(sudoku_puzzle_t *puzzle) {
             }
         }
     } while(changed);
-
-    printf("done solving!\n");
-    json_dumpf(sudoku_puzzle_to_json(puzzle), stdout, 0);
-    printf("\n");
 
     return sudoku_puzzle_solved(puzzle);
 }
