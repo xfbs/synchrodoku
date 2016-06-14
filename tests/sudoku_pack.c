56e7c67207eaac7ba05f0c6be01d6cd4a9151eb9
Patrick Elsen <pelsen@xfbs.net>
2016-06-14 11:26:40 +0200

    expands tests for sudoku_pack

--- tests/sudoku_pack.c
+++ tests/sudoku_pack.c
@@ -32,7 +32,7 @@ TEST(pack_preserves_bits) {
 }
 
 TEST(pack_works_with_random) {
-    sudoku_puzzle_t puzzle;
+    sudoku_puzzle_t puzzle, unpacked;
 
     puzzle = sudoku_puzzle_new((int[9][9]){
             {1, 2, 3, 4, 5, 6, 7, 8, 9},
@@ -45,10 +45,30 @@ TEST(pack_works_with_random) {
             {2, 3, 6, 9, 0, 0, 0, 7, 0},
             {5, 1, 2, 9, 8, 5, 5, 8, 7}});
 
+    sudoku_puzzle_pack(packed, &puzzle);
+    unpacked = sudoku_puzzle_unpack(packed);
+    assertEquals(sudoku_puzzle_equals_strict(&puzzle, &unpacked), true);
+
+    // modify puzzle slightly
     *(sudoku_puzzle_cell(&puzzle, 2, 2)) = sudoku_cell_new((int[]){3, 5, 9, 0});
     *(sudoku_puzzle_cell(&puzzle, 5, 3)) = sudoku_cell_new((int[]){8, 4, 0});
 
     sudoku_puzzle_pack(packed, &puzzle);
-    sudoku_puzzle_t unpacked = sudoku_puzzle_unpack(packed);
+    unpacked = sudoku_puzzle_unpack(packed);
+    assertEquals(sudoku_puzzle_equals_strict(&puzzle, &unpacked), true);
+
+    puzzle = sudoku_puzzle_new((int[9][9]){
+            {1, 2, 3, 4, 5, 6, 7, 8, 9},
+            {2, 3, 2, 3, 0, 7, 7, 7, 5},
+            {2, 9, 6, 3, 9, 5, 2, 3, 5},
+            {7, 3, 6, 2, 4, 2, 5, 1, 1},
+            {5, 2, 2, -1, 4, 9, 2, 3, 4},
+            {2, 3, 6, 2, 2, 0, 7, 8, 5},
+            {7, 1, 8, 8, 8, 8, -1, 2, 7},
+            {2, 3, 6, 9, 2, 0, 2, 7, 0},
+            {5, 1, 2, -1, 8, 6, 6, 8, 7}});
+
+    sudoku_puzzle_pack(packed, &puzzle);
+    unpacked = sudoku_puzzle_unpack(packed);
     assertEquals(sudoku_puzzle_equals_strict(&puzzle, &unpacked), true);
 }
