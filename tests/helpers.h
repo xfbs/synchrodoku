6265c3342c00ae9a9d212a114291f77b79204395
Patrick Elsen <pelsen@xfbs.net>
2016-06-15 16:26:31 +0200

    adds messages api to tests helpers

--- tests/helpers.h
+++ tests/helpers.h
@@ -1,6 +1,7 @@
 #include "cu/cu.h"
 #include "solver.h"
 #include "sudoku.h"
+#include "messages.h"
 
 bool sudoku_puzzle_equals_strict(const sudoku_puzzle_t *a, const sudoku_puzzle_t *b);
 bool sudoku_puzzle_equals(const sudoku_puzzle_t *a, const sudoku_puzzle_t *b);
